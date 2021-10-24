6897ac523e9267a7bb3a62ad8afada8860cfe3ff
vish <vishgirase95@gmail.com>
2021-10-24 14:17:08 +0530

    [vishal] added Power of 2

---
+++ BasicPrograming/ConsoleApp1/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+
+     
+
+        public static void TwoPower()
+        {
+            int InputPower =Convert.ToInt32( Console.ReadLine());
+
+            for (int i = 1; i <= InputPower; i++) {
+
+                Console.WriteLine(Math.Pow(2, i));
+
+ 
+              
+            
+            }
+
+        }
+
+    }
+}
--- BasicPrograming/ConsoleApp1/Program.cs
+++ BasicPrograming/ConsoleApp1/Program.cs
@@ -3,75 +3,20 @@
 namespace ConsoleApp1
 {
 
-
    
     class Program
     {
 
-            static int CorrectYear = 999;
-
-        public static void CheckYear() {
-            int InputYear = Convert.ToInt32(Console.ReadLine());
-
-            if (InputYear < 999)
-            {
-                Console.WriteLine("Please Enter Year in YYYY form.");
-
-            }
-            else
-            {
-                CorrectYear = InputYear;
-            }
           
-        }
-
-
-        public static void CheckLeapYear()
-        {
-
-            if (CorrectYear > 999) 
-
-            if (CorrectYear % 4 == 0)
-            {
-                if (CorrectYear % 100 == 0)
-                {
-                    if (CorrectYear % 400 == 0)
-                    {
-                        Console.WriteLine($"{CorrectYear} is leap Year");
-
-                    }
-                    else
-                    {
-                        Console.WriteLine($"{CorrectYear} is not leap Year");
-
-                    }
-                }
-                else
-                {
-                    Console.WriteLine($"{CorrectYear} is leap Year");
-
-
-                }
-            }
-            else
-            {
-                Console.WriteLine($"{CorrectYear} is not leap Year");
-            }
-        }
-
-
 
          static void Main(string[] args)
         {
             
-            Console.WriteLine("Enter the Year to check if its Leap Year :");
+            Console.WriteLine("Enter the Power of 2 :");
+            Class1.TwoPower();
+          
            
-            CheckYear();
-
-            CheckLeapYear();
-
-
-
+     
 
         }
     }
