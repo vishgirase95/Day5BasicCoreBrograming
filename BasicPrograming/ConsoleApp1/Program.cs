5983e835a8734e4d970e04b271542cb9abecbd78
vish <vishgirase95@gmail.com>
2021-10-24 10:52:23 +0530

    [vishal] added Flip coin percentage

---
+++ BasicPrograming/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentFlip = 1;
+            Console.WriteLine("Enter The number of Flip :");
+            int flipNumber = Convert.ToInt32(Console.ReadLine());
+            int HeadCount = 0;
+            int TailCount = 0;
+
+            int i = 0;
+            while (i <= flipNumber)
+            {
+                Random flip = new Random();
+                int  RandomNumber= flip.Next(1, 3);
+
+
+                if (currentFlip == RandomNumber)
+                {
+                    Console.WriteLine("Head");
+                    HeadCount++;
+                }
+                else
+                {
+                    Console.WriteLine("Tail");
+                    TailCount++;
+                }
+                i++;
+            }
+
+            int PercentageOfHead = (100 * HeadCount) / flipNumber;
+            int PercentageOfTail =100- PercentageOfHead;
+            Console.WriteLine($"The Percentage of Head count {PercentageOfHead}% and the percentage of tail count is {PercentageOfTail}%");        
+        }
+    }
+}
